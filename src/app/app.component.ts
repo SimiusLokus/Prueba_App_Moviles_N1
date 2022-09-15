213d0e74002ba0da39a3db587932a4a0c738228c
Daniel_Novoa <da.novoa@duocuc.cl>
2022-09-15 10:07:16 -0300

    Pasaporflojo

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Prueba_Daniel_Novoa_006D';
+}
